4d58069986a7bcb433e5dce652266f8482e8cfba
Peter Kofler <peter.kofler@code-cop.org>
2020-01-22 23:23:02 +0100

    Remove tests.

--- JavaScript/test/rabatt_rechner_test.js
+++ JavaScript/test/rabatt_rechner_test.js
@@ -11,39 +11,7 @@ describe("Rabatt Rechner", function() {
   });
 
   it("should foo", function() {
-    expect(true).to.equal(true);
-  });
-
-  it("shouldGiveNoDiscountForPurchaseBelow1000Eur", function() {
-    var purchaseBelow1000Eur = new bigdecimal.BigDecimal(275);
-
-    var discount = calculator.discountFor(purchaseBelow1000Eur);
-
-    expect(discount.compareTo(new bigdecimal.BigDecimal(0))).to.equal(0);
-  });
-
-  it("shouldGiveNoDiscountForPurchaseJust1000Eur", function() {
-    var purchaseBelow1000Eur = new bigdecimal.BigDecimal(999);
-
-    var discount = calculator.discountFor(purchaseBelow1000Eur);
-
-    expect(discount.compareTo(new bigdecimal.BigDecimal(0))).to.equal(0);
-  });
-
-  it("shouldGive3PercentDiscountForPurchaseAbove1000Eur", function() {
-    var purchaseAbove1000Eur = new bigdecimal.BigDecimal(2000);
-
-    var discount = calculator.discountFor(purchaseAbove1000Eur);
-
-    expect(discount.compareTo(new bigdecimal.BigDecimal(60))).to.equal(0);
-  });
-
-  it("shouldGive3PercentDiscountForPurchaseOfExactly1000Eur", function() {
-    var purchaseOfExactly1000Eur = new bigdecimal.BigDecimal(1000);
-
-    var discount = calculator.discountFor(purchaseOfExactly1000Eur);
-
-    expect(discount.compareTo(new bigdecimal.BigDecimal(30))).to.equal(0);
+    expect(true).to.equal(false);
   });
 
 });
