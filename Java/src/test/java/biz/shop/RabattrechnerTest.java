f092cf8da83930f7390e1409e78a595ba3a4cb50
Peter Kofler <peter.kofler@code-cop.org>
2020-01-22 14:54:09 +0100

    Show sample of usage in test.

--- Java/src/test/java/biz/shop/RabattrechnerTest.java
+++ Java/src/test/java/biz/shop/RabattrechnerTest.java
@@ -3,11 +3,19 @@ package biz.shop;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RabattrechnerTest {
 
     private Rabattrechner rechner = new Rabattrechner();
+
+    @BeforeEach
+    void setUpDiscount() {
+        rechner.addDiscountLevel(new BigDecimal(1000), 3);
+    }
 
     @Test
     void shouldFoo() {
