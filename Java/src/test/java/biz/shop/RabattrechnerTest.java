c374b4f39aaec01157bbe0107940240070a802b3
Peter Kofler <peter.kofler@code-cop.org>
2020-01-22 14:49:05 +0100

    Remove tests.

--- Java/src/test/java/biz/shop/RabattrechnerTest.java
+++ Java/src/test/java/biz/shop/RabattrechnerTest.java
@@ -1,64 +1,17 @@
 package biz.shop;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-import java.math.BigDecimal;
-
-import org.hamcrest.Matcher;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RabattrechnerTest {
 
     private Rabattrechner rechner = new Rabattrechner();
 
-    @BeforeEach
-    void setUpDiscount() {
-        rechner.addDiscountLevel(new BigDecimal(1000), 3);
-    }
-
     @Test
-    void shouldGiveNoRabattForPurchaseBelow1000Eur() {
-        BigDecimal purchaseBelow1000Eur = toMoney(275);
-
-        BigDecimal rabatt = rechner.calcRabattFor(purchaseBelow1000Eur);
-
-        assertThat(rabatt, is(0));
-    }
-
-    @Test
-    void shouldGiveNoRabattForPurchaseJust1000Eur() {
-        BigDecimal purchaseBelow1000Eur = toMoney(999);
-
-        BigDecimal rabatt = rechner.calcRabattFor(purchaseBelow1000Eur);
-
-        assertThat(rabatt, is(0));
-    }
-
-    @Test
-    void shouldGive3PercentRabattForPurchaseAbove1000Eur() {
-        BigDecimal purchaseAbove1000Eur = toMoney(2000);
-
-        BigDecimal rabatt = rechner.calcRabattFor(purchaseAbove1000Eur);
-
-        assertThat(rabatt, is(60));
-    }
-
-    @Test
-    void shouldGive3PercentRabattForPurchaseOfExactly1000Eur() {
-        BigDecimal purchaseOfExactly1000Eur = toMoney(1000);
-
-        BigDecimal rabatt = rechner.calcRabattFor(purchaseOfExactly1000Eur);
-
-        assertThat(rabatt, is(30));
-    }
-
-    private static BigDecimal toMoney(int euros) {
-        return new BigDecimal(euros);
-    }
-
-    private Matcher<? super BigDecimal> is(int euros) {
-        return BigDecimalMatchers.is(toMoney(euros));
+    void shouldFoo() {
+        assertThat(true, is(false));
     }
 
 }
