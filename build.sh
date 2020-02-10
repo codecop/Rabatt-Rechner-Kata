f9287622edda8e8b1a6b6e32465812e39fd1266a
Peter Kofler <peter.kofler@code-cop.org>
2020-02-10 18:41:29 +0100

    Add build script for Linux.

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo Build the package for participants.
+
+cd Java
+rm -rf lib
+# see http://stackoverflow.com/questions/7908090/downloading-all-maven-dependencies-to-a-directory-not-in-repository
+mvn dependency:copy-dependencies -DoutputDirectory=lib -Dmdep.useRepositoryLayout=true
+mvn dependency:copy-dependencies -DoutputDirectory=lib -Dmdep.useRepositoryLayout=true -Dclassifier=sources
+mvn eclipse:clean
+mvn eclipse:eclipse
+cd ..
+ant rewrite
+
+cd JavaScript
+npm install
+cd ..
+
+ant zip
+# ant clean
