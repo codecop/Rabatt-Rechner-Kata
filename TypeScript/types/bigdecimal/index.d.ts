018c81d2c21793f8d55d23841a312fa8eef08f67
Peter Kofler <peter.kofler@code-cop.org>
2022-04-06 14:33:54 +0200

    Allow string in Bigdecimal too.

--- TypeScript/types/bigdecimal/index.d.ts
+++ TypeScript/types/bigdecimal/index.d.ts
@@ -2,7 +2,7 @@ declare module 'bigdecimal' {
     // minimal, incomplete typing
 
     export class BigDecimal {
-        constructor(n: number);
+        constructor(n: number | string);
         add(o: BigDecimal): BigDecimal;
         multiply(o: BigDecimal): BigDecimal;
         divide(o: BigDecimal): BigDecimal;
