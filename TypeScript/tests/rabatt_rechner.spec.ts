ddfc484d08c5a031b3b6cbb878ed073f6333acd0
Peter Kofler <peter.kofler@code-cop.org>
2023-09-27 15:55:58 +0200

    Using code completion in code and comment. Meh.

--- TypeScript/tests/rabatt_rechner.spec.ts
+++ TypeScript/tests/rabatt_rechner.spec.ts
@@ -9,8 +9,19 @@ describe("Rabatt Rechner", () => {
     calculator.addDiscountLevel(new BigDecimal(1000), 3);
   });
 
-  it("should foo", () => {
-    expect(true).toBe(false);
-  });
+  it("should discount for 1000", () => {
+    expect(calculator.discountFor(new BigDecimal(1000))).toEqual(new BigDecimal(30));
+    // assumes 10 percent instead of 5
+  })
+
+  it("should discount for 2000", () => {
+    expect(calculator.discountFor(new BigDecimal(2000))).toEqual(new BigDecimal(60));
+  })
+
+  // list tests cases for RabattRechner as list in comments:
+  // * should discount for 1000
+  // * should discount for 2000
+  // * should discount for 3000 WTF
+  
 
 });
