a8ef3d7fa809e41169ef6091e4512f9ba944472a
Peter Kofler <peter.kofler@code-cop.org>
2023-09-27 16:19:14 +0200

    Tests after a view interactions.

--- TypeScript/tests/rabatt_rechner.spec.ts
+++ TypeScript/tests/rabatt_rechner.spec.ts
@@ -6,22 +6,68 @@ describe("Rabatt Rechner", () => {
 
   beforeEach(() => {
     calculator = new RabattRechner();
-    calculator.addDiscountLevel(new BigDecimal(1000), 3);
   });
 
-  it("should discount for 1000", () => {
-    expect(calculator.discountFor(new BigDecimal(1000))).toEqual(new BigDecimal(30));
-    // assumes 10 percent instead of 5
-  })
+  const noDiscount = new BigDecimal(0);
 
-  it("should discount for 2000", () => {
-    expect(calculator.discountFor(new BigDecimal(2000))).toEqual(new BigDecimal(60));
-  })
+  it("should return noDiscount when the purchase amount is 0", () => {
+    // Arrange
+    const purchase = new BigDecimal(0);
 
-  // list tests cases for RabattRechner as list in comments:
-  // * should discount for 1000
-  // * should discount for 2000
-  // * should discount for 3000 WTF
-  
+    // Act
+    const discount = calculator.discountFor(purchase);
+
+    // Assert
+    expect(discount).toEqual(noDiscount);
+  });
+
+  it("should return noDiscount when no discount levels have been added", () => {
+    // Arrange
+    const purchase = new BigDecimal(1000);
+
+    // Act
+    const discount = calculator.discountFor(purchase);
+
+    // Assert
+    expect(discount).toEqual(noDiscount);
+  });
+
+  it("should return the correct discount amount when the purchase amount is equal to a discount level threshold", () => {
+    // Arrange
+    calculator.addDiscountLevel(new BigDecimal(1000), 10);
+    const purchase = new BigDecimal(1000);
+
+    // Act
+    const discount = calculator.discountFor(purchase);
+
+    // Assert
+    expect(discount).toEqual(purchase.multiply(new BigDecimal(0.1)));
+  });
+
+  it("should return the correct discount amount when the purchase amount is greater than a discount level threshold", () => {
+    // Arrange
+    calculator.addDiscountLevel(new BigDecimal(1000), 10);
+    calculator.addDiscountLevel(new BigDecimal(2000), 20);
+    const purchase = new BigDecimal(2500);
+
+    // Act
+    const discount = calculator.discountFor(purchase);
+
+    // Assert
+    expect(discount).toEqual(purchase.multiply(new BigDecimal(0.2)));
+  });
+
+  it("should return the correct discount amount when the purchase amount is less than all discount level thresholds", () => {
+    // Arrange
+    calculator.addDiscountLevel(new BigDecimal(1000), 10);
+    calculator.addDiscountLevel(new BigDecimal(2000), 20);
+    const purchase = new BigDecimal(500);
+
+    // Act
+    const discount = calculator.discountFor(purchase);
+
+    // Assert
+    expect(discount).toEqual(noDiscount);
+  });
 
 });
